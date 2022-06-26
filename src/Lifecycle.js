14a27d8e8c47a5b19ab0fcf08599a983845f6517
jaehee <jaehee23@naver.com>
2022-06-26 23:07:26 +0900

    LifeCycle 1

---
+++ src/Lifecycle.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+
+const Lifecycle = ()=> {
+
+    const [count, setCount] = useState(0);
+    const [text, setText] = useState("");
+
+    useEffect( ()=>{
+        console.log("Mount!!");
+    }, []);
+
+    useEffect(()=>{
+        console.log("Update!")
+    });
+
+    useEffect(()=>{
+        console.log(`count is update : ${count}`);
+    }, [count]);
+
+    useEffect(()=> {
+        console.log(`text is update : ${text}`);
+    }, [text])
+
+    return <div style={{ padding: 20 }}>
+        <div>
+            {count}
+            <button onClick={()=>setCount(count + 1)}>+</button>
+        </div>
+        <div>
+            <input value={text}
+                   onChange={(e)=> setText(e.target.value)} />
+        </div>
+    </div>;
+};
+
+export  default Lifecycle;
