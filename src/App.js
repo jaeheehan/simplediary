cdc2a252a3c9bbf4dca6622ccdd6687483683bde
jaehee <jaehee23@naver.com>
2022-07-01 06:34:55 +0900

    api

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
 import './App.css';
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import {useState, useRef} from "react";
+import {useState, useRef, useEffect} from "react";
 import Lifecycle from "./Lifecycle";
 
-const dummyList = [
-    {
-        id:1,
-        author: "이정환",
-        content: "하이 1",
-        emotion: 5,
-        created_date: new Date().getTime()
-    },
-    {
-        id:2,
-        author: "홍길동",
-        content: "하이 2",
-        emotion: 2,
-        created_date: new Date().getTime()
-    },
-    {
-        id:3,
-        author: "아무개",
-        content: "하이 3",
-        emotion: 1,
-        created_date: new Date().getTime()
-    }
-]
-
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
     const [data, setData ] = useState([]);
 
     const dataId = useRef(0);
+
+    const getData = async() => {
+        const res = await fetch(
+            'https://jsonplaceholder.typicode.com/comments'
+        ).then((res)=>res.json());
+
+        const initData = res.slice(0, 20).map((it)=>{
+            return {
+                author: it.email,
+                content: it.body,
+                emotion: Math.floor(Math.random() * 5) +1,
+                created_date : new Date().getTime(),
+                id: dataId.current++
+            }
+        });
+
+        setData(initData);
+
+    }
+
+    useEffect(()=>{
+        getData();
+    }, []);
 
     const onCreate = (author, content, emotion) => {
         const created_date = new Date().getTime();
@@ -60,7 +60,6 @@ function App() {
 
     return (
         <div className="App">
-            <Lifecycle/>
             <DiaryEditor onCreate={onCreate}/>
             <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit}/>
         </div>
