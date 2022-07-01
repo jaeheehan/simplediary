b7217bc58858ba59c303d3a10f5953771bd5018a
jaehee <jaehee23@naver.com>
2022-07-01 23:24:59 +0900

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import './App.css';
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import {useState, useRef, useEffect} from "react";
-import Lifecycle from "./Lifecycle";
-
-// https://jsonplaceholder.typicode.com/comments
+import {useState, useRef, useEffect, useMemo} from "react";
 
 function App() {
     const [data, setData ] = useState([]);
@@ -58,9 +55,24 @@ function App() {
         )
     }
 
+    const getDiaryAnalysis = useMemo(() => {
+        console.log("일기 분석 시작");
+
+        const goodCount = data.filter((it)=>it.emotion >= 3).length;
+        const badCount = data.length - goodCount;
+        const goodRatio = (goodCount / data.length) * 100;
+        return {goodCount, badCount, goodRatio};
+    }, [data.length]);
+
+    const {goodCount, badCount, goodRatio} = getDiaryAnalysis;
+
     return (
         <div className="App">
             <DiaryEditor onCreate={onCreate}/>
+            <div>전체 일기 : {data.length}</div>
+            <div>기분 좋은 일기 개수 : {goodCount} </div>
+            <div>기분 나쁜 일기 개수 : {badCount} </div>
+            <div>기분 좋은 일기 비율 : {goodRatio} </div>
             <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit}/>
         </div>
     );
